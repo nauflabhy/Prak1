028916da2c4ec9fb42acad435567e2a77e3ec4c0
nauflabhy <n4uf4l.ku@gmail.com>
2026-09-24 15:13:14 +0700

    Perubahan name pada line 244

--- app/src/main/java/com/example/prak1/MainActivity.kt
+++ app/src/main/java/com/example/prak1/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Prak1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "jejojio",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
